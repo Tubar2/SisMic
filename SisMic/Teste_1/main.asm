65afdf469b874a6ca451c4d205b671baa00ae02c
ricardosantos <rick.giometti@hotmail.com>
2020-08-21 10:44:43 -0300

    Create Teste_1

---
+++ SisMic/Teste_1/main.asm
@@ -0,0 +1,54 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+main:
+		mov.w 	#var1, R4
+		mov.w 	var1, R5
+		mov.b	@R4+, R6
+		mov.b	4(R4), -0X1C(R5)
+		mov.w	&var3, R7
+
+		jmp 	$
+		nop
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+
+;-------------------------------------------------------------------------------
+; Memória RAM
+;-------------------------------------------------------------------------------
+			.data
+var1:		.word		0x2420
+var2:		.byte		-1
+var3:		.word		0xABCD
