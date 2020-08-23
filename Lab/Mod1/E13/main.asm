68740298872de4f7ab7d8ebb0aaa840f4b97518e
ricardosantos <rick.giometti@hotmail.com>
2020-08-23 18:49:01 -0300

    Finish exercise 13 and added it's flow chart

--- Lab/Mod1/E13/main.asm
+++ Lab/Mod1/E13/main.asm
@@ -24,6 +24,11 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ; Main loop here
 ;-------------------------------------------------------------------------------
 
+;R6 -> i
+;R7 -> j
+;R8 -> LookedUp val 2
+;R9 -> LookedUp val 1
+
 main:
 		mov 	#vetor, r6
 		call 	#rom_arab
@@ -31,12 +36,78 @@ main:
 		nop
 
 rom_arab:
+		mov 	r6, r4			;Save the start of the vector on R4
+		clr 	r5				;ANSWER
+rewind:							;Rewind R6 to the end of roman vector
+		inc 	r6
+		cmp.b 	#0, 0(r6)
+		jne 	rewind
+algo:
+		dec 	r6 ;Talvez aqui ou no loop
+		cmp 	r6, r4
+		jeq 	exit1
+		call 	#lookUp
+		mov 	r8, r9
+		dec 	r6
+		call 	#lookUp
+
+		cmp 	r9, r8
+		jhs 	rollback
+exit2:
+		add 	r9, r5
+		cmp 	r6, r4
+		jne 	algo
+		ret
+exit1:
+		call 	#lookUp
+		add 	r8, r5
+		ret
+
+rollback:
+		inc 	r6
+		jmp 	exit2
 
 
+lookUp:
+		cmp.b 	#'I', 0(r6)
+		jeq 	caseI
+		cmp.b 	#'V', 0(r6)
+		jeq 	caseV
+		cmp.b 	#'X', 0(r6)
+		jeq 	caseX
+		cmp.b 	#'L', 0(r6)
+		jeq 	caseL
+		cmp.b 	#'C', 0(r6)
+		jeq 	caseC
+		cmp.b 	#'D', 0(r6)
+		jeq 	caseD
+		cmp.b 	#'M', 0(r6)
+		jeq 	caseM
+caseI:
+		mov 	#1, r8
+		ret
+caseV:
+		mov 	#5, r8
+		ret
+caseX:
+		mov 	#10, r8
+		ret
+caseL:
+		mov 	#50, r8
+		ret
+caseC:
+		mov 	#100, r8
+		ret
+caseD:
+		mov 	#500, r8
+		ret
+caseM:
+		mov 	#1000, r8
+		ret
 
 
 				.data
-vetor: 			.byte 	"MMXIX"
+vetor: 			.byte 	"MMXIV", 0
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
