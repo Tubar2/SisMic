04496c09fc601ba9fd0263aa4454f2ce520bb5fb
Ricardo <46869885+Tubar2@users.noreply.github.com>
2020-10-10 00:13:58 -0300

    Finish Exercise 14

---
+++ Lab/Mod2/E14/main.c
@@ -0,0 +1,67 @@
+#include <msp430.h> 
+#include "libs/gpio.h"
+#include "libs/timer.h"
+
+/**
+ * main.c
+ */
+
+#define STEP 1048
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	pinConfig(1, 0, output);    // LED01 vermelho como saída
+	pinConfig(2, 1, inPullUp);  // Pino da Esquerda
+	pinConfig(1, 1, inPullUp);  // Pino da Direita
+
+	pinConfig(1, 3, output);    //Pino 1.3 como saída
+
+	P1SEL |= BIT3;              // Conecta o timer A0 e contador 2 (CCR2) ao pino 1.3
+
+	const uint16_t n = 20971;   // Frequência de 50Hz de piscagem n = Fclk/50Hz
+	const uint16_t n2 = 10486;  // Duty cycle de 50% n2 = 0.5 * n
+
+	// Registradores CTL, CCR0 e CCR2
+	uint16_t * timerCTL = getTimerRegstr(TA_0, CTL, 0);
+	uint16_t * timerCCR0 = getTimerRegstr(TA_0, CCR, 0);
+	uint16_t * timerCCR2 = getTimerRegstr(TA_0, CCR, 2);
+
+	// Configurando timerA0 com SMCLK e contagem UP
+	configTimer(timerCTL, SMCLK, UP, 0);
+
+    // Interrupções em n e n2
+	setInterruptionAt(timerCCR0, n);
+	setInterruptionAt(timerCCR2, n2);
+
+	// Configurando
+	TA0CCTL2 = OUTMOD_7;
+	uint16_t * timerCCTL0 = getTimerRegstr(TA_0, CCTL, 0);
+
+
+	uint8_t sEsq_prev = 1;
+	uint8_t sDir_prev = 1;
+	while(1){
+	    // Acende o LED
+	    while(!(*timerCCTL0 & CCIFG));
+	    *timerCCTL0 &= ~CCIFG;
+	    P1OUT |= BIT0;
+
+	    sEsq_prev = digitalRead(2,1);
+	    while (!sEsq_prev) {
+	        sEsq_prev = digitalRead(2, 1);
+	        *timerCCR2 += STEP;
+	    }
+
+	    sDir_prev = digitalRead(1,1);
+	    while (!sDir_prev) {
+	        sDir_prev = digitalRead(1,1);
+	        *timerCCR2 -= STEP;
+
+	    }
+
+	}
+
+}
+
+
